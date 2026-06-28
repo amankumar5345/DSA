d95f7316508a3bf2a6c9cd767e28f201e82b4505
Aman Kumar <159311357+amankumar5345@users.noreply.github.com>
2026-06-28 13:54:52 +0530

    Time: 4 ms (1.81%) | Memory: 7.9 MB (7.86%) - LeetSync

---
+++ 263-ugly-number/ugly-number.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n <= 0)
+        return false;
+        for (int i = 2; 1LL * i * i <= n; i++) {
+            while (n % i == 0) {
+                if (i != 2 && i != 3 && i != 5)
+                    return false;
+                n /= i;
+            }
+        }
+
+        return (n == 1 || n == 2 || n == 3 || n == 5);
+
+    }
+};
